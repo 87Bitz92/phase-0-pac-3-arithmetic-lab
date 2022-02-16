5f7e1aab3b6d6bcd45070a08c9db4c54318cdc6d
Christina <itaraujo8720@gmail.com>
2022-02-16 00:09:07 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+function add() {
+    return (4 + 80)
+};
+
+function subtract() {
+    return (60 - 50)
+}; 
+
+function multiply() {
+    return (5 * 10)
+}; 
+
+function divide() {
+    return (8 / 100)
+}; 
+
+function add(a, b){
+    return (a += b)
+};
+
+function subtract(b, a) {
+    return (b -= a)
+};
+
+function multiply(a, b) {
+    return (a *= b)
+};
+
+function divide(a, b) {
+    return (a /= b)
+};
+
+
+let n = 10;
+
+function increment(n) {
+    return (++n)
+};
+
+function decrement(n) {
+    return (--n)
+};
+
+function makeInt(n) {
+    return (parseInt(n, 10))
+};
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+};
